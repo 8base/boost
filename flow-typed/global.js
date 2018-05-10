f47bfb0d562a383314ef4e9cf87a783b0fa7d042
Ivan Pinchuk <ivan.pinchuk@8base.com>
2018-05-10 14:34:29 +0300

    test(core): add jest adapter and global functions

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -16,6 +16,11 @@ declare var Choose: React$ComponentType<$Exact<{ children: React$Node }>>;
 declare var When: React$ComponentType<$Exact<{ condition: boolean, children: React$Node }>>;
 declare var Otherwise: React$ComponentType<$Exact<{ children: React$Node }>>;
 
+/** test utils declarations */
+declare var render: typeof __enzume.render;
+declare var mount: typeof __enzume.mount;
+declare var shallow: typeof __enzume.shallow;
+
 /** hocs helpers */
 type UnaryFn<A, R> = (a: A) => R
 
@@ -25,7 +30,7 @@ declare type HOC<Base, Enhanced> = UnaryFn<
 >;
 
 declare type HOCProps<BaseAdd, T: Object = *> = HOC<{| ...$Exact<BaseAdd>, ...$Exact<T> |}, T>
-declare type HOCPropsInOut<BaseAdd, Enhanced: Object = *> =  HOC<{ ...$Exact<Enhanced>, ...BaseAdd }, Enhanced>;
+declare type HOCPropsInOut<BaseAdd, Enhanced: Object = *> = HOC<{ ...$Exact<Enhanced>, ...BaseAdd }, Enhanced>;
 
 // eslint-disable-next-line no-unused-vars
 type HOCBase_<A, B, C: HOC<A, B>> = A;
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,13 @@
 import * as emotion from 'emotion';
 import { createSerializer } from 'jest-emotion';
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-expect.addSnapshotSerializer(createSerializer(emotion))
+configure({ adapter: new Adapter() });
+
+// make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+expect.addSnapshotSerializer(createSerializer(emotion));

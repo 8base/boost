d147fedadd876172332017e292a10fb088dc9fe1
ej9x <ivan.pinchuk@8base.com>
2018-05-17 17:08:55 +0300

    feat(atoms): expand Icon component. add babel svg plugin

--- src/atoms/typography/Icon/Icon.stories.js
+++ src/atoms/typography/Icon/Icon.stories.js
@@ -3,11 +3,26 @@ import React from 'react';
 export default (asStory) => {
   asStory('ATOMS/TYPOGRAPHY/Icon', module, (story, { Icon }) => {
     story
-      .add('with default modifiers', () => (
-        <React.Fragment>
+      .add('all icons', () => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
           <Icon name="ChevronDown" />
-        </React.Fragment>
+          <Icon name="Check" />
+        </div>
+      ))
+      .add('with custom color', () => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Icon name="Check" color="primary" />
+          <Icon name="Check" color="green" />
+          <Icon name="Check" color="blue" />
+        </div>
+      ))
+      .add('with custom sizes', () => (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Icon name="Check" size="xs" />
+          <Icon name="Check" size="sm" />
+          <Icon name="Check" size="md" />
+          <Icon name="Check" size="lg" />
+        </div>
       ));
   });
 };
-
--- src/atoms/typography/Icon/glyphs/index.js
+++ src/atoms/typography/Icon/glyphs/index.js
@@ -1,1 +1,12 @@
-export { ChevronDown } from './ChevronDown';
+// @flow
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import Check from './Check.svg';
+import ChevronDown from './ChevronDown.svg';
+
+export {
+  ChevronDown,
+  Check,
+};
+

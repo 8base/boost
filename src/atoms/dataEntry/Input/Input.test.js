6f17d738803b479e372f217cdbbaf8bca1c44cc5
ej9x <ivan.pinchuk@8base.com>
2018-05-17 17:10:11 +0300

    test(atoms): add tests for the input component

---
+++ src/atoms/dataEntry/Input/Input.test.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react';
+import { Input } from './Input';
+
+describe('<Input />', () => {
+  it('should call onCahnge with text value', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(<Input onChange={ onChange } />);
+
+    wrapper.find('input').simulate('change', { target: { value: 'val' }});
+    expect(onChange.mock.calls[0][0]).toBe('val');
+  });
+
+  it('should call onChange with number value', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(<Input onChange={ onChange } type="number" />);
+
+    wrapper.find('input').simulate('change', { target: { value: 'val' }});
+    expect(onChange.mock.calls[0][0]).toBe(undefined);
+
+    wrapper.find('input').simulate('change', { target: { value: '42' }});
+    expect(onChange.mock.calls[1][0]).toBe(42);
+    expect(typeof onChange.mock.calls[1][0]).toBe('number');
+  });
+});

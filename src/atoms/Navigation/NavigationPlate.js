22bdf57f0b3d32e44a83f01c661f7f5ae90fe6d9
zouxuoz <zouxuoz@gmail.com>
2018-08-16 18:08:36 +0300

    feat(navigation): new navigation

--- src/atoms/Navigation/NavigationPlate.js
+++ src/atoms/Navigation/NavigationPlate.js
@@ -1,30 +1,53 @@
 import React from 'react';
+import fp from 'lodash/fp';
 
 import { createStyledTag, createTheme } from '../../utils';
+import { PALETTE } from '../../theme';
 
 type NavigationPlateProps = {|
   children?: React.Node,
+  color: $Keys<typeof PALETTE>
 |};
 
 const name = 'navigationPlate';
 
 const theme = createTheme(name, {
   modifiers: {
-  },
-  defaults: {
+    color: fp.mapValues((color) => ({
+      backgroundColor: color,
+    }), PALETTE),
   },
 });
 
 const StyledTag = createStyledTag(name, {
   display: 'flex',
   flexDirection: 'column',
+  height: '100%',
+  width: '6rem',
+
+  '&:hover': {
+    width: 'auto',
+
+    '.NavigationItem-label': {
+      display: 'flex',
+    },
+  },
 });
 
 function NavigationPlate({
   children,
+  color,
   ...rest
   }: NavigationPlateProps) {
-  return <StyledTag { ...rest } tagName="nav">{ children }</StyledTag>;
+  return (
+    <StyledTag { ...rest } color={ color } tagName="nav">
+      {
+        React.Children.map(children, child =>
+          React.cloneElement(child, { color: child.props.color || color }),
+        )
+      }
+    </StyledTag>
+  );
 }
 
 export { NavigationPlate, theme };

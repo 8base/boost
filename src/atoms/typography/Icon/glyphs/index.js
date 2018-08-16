04d98cf2e5b09b3450ada7947b8d1d23d67ac67e
zouxuoz <zouxuoz@gmail.com>
2018-08-16 17:38:16 +0300

    feat(icon): add icons

--- src/atoms/typography/Icon/glyphs/index.js
+++ src/atoms/typography/Icon/glyphs/index.js
@@ -2,17 +2,31 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-import Mail from './Mail.svg';
 import Check from './Check.svg';
 import ChevronDown from './ChevronDown.svg';
+import CloudDatabase from './Cloud-Database.svg';
+import FirewallOn from './Firewall-On.svg';
+import Gear from './Gear.svg';
+import Group from './Group.svg';
+import Lock from './Lock.svg';
+import Mail from './Mail.svg';
+import MapPin from './MapPin.svg';
 import Search from './Search.svg';
 import Trashcan from './Trashcan.svg';
+import SpeedMeter from './Speed-Meter.svg';
 
 export {
-  Mail,
   Check,
   ChevronDown,
+  CloudDatabase,
+  FirewallOn,
+  Gear,
+  Group,
+  Lock,
+  Mail,
+  MapPin,
   Search,
   Trashcan,
+  SpeedMeter,
 };
 

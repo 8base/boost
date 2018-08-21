e069f06fa49489964b901b1ec5bf753de5d2706a
Andrei Firsov <andrei.firsov@8base.com>
2018-08-21 14:24:21 +0300

    feat(Form): adds ControlErrorTag to export

--- src/atoms/dataEntry/Form/index.js
+++ src/atoms/dataEntry/Form/index.js
@@ -1,4 +1,4 @@
-import { FormField as Field, theme as formFieldTheme } from './FormField';
+import { FormField as Field, theme as formFieldTheme, ControlErrorTag } from './FormField';
 import { FormPlate as Plate, theme as formPlateTheme } from './FormPlate';
 import { FormSection as Section } from './FormSection';
 import { FormSectionTitle as SectionTitle, theme as formSectionTitleTheme } from './FormSectionTitle';
@@ -8,6 +8,7 @@ const Form = {
   Plate,
   Section,
   SectionTitle,
+  ControlErrorTag,
 };
 
 const theme = {

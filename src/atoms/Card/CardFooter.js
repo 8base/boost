56406b1a82591db4c8a7e0221295d4a6f54d01d2
Andrei Firsov <andrei.firsov@8base.com>
2018-08-28 18:17:34 +0300

    fix(Card): adds prohibition to shrink for header and footer

--- src/atoms/Card/CardFooter.js
+++ src/atoms/Card/CardFooter.js
@@ -14,6 +14,7 @@ const name = 'cardFooter';
 const cardFooterTheme = createTheme(name, (colors: *): * => ({
   cardFooter: {
     borderTop: `1px solid ${colors.PRIMARY_BORDER_COLOR}`,
+    flexShrink: 0,
   },
   modifiers: {
     ...offsetModifier,

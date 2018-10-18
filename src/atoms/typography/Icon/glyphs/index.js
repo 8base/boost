d38e423b7dd8e151b02a83ed20524d6a70cfad88
ej9x <ivan.pinchuk@8base.com>
2018-10-18 14:33:46 +0300

    feat(atoms): add home and help icons

--- src/atoms/typography/Icon/glyphs/index.js
+++ src/atoms/typography/Icon/glyphs/index.js
@@ -5,45 +5,49 @@ import Alert from './Alert.svg';
 import Check from './Check.svg';
 import ChevronDown from './ChevronDown.svg';
 import CloudDatabase from './Cloud-Database.svg';
+import Delete from './Delete.svg';
 import Diagram from './Diagram.svg';
 import Dots from './Dots.svg';
-import GraphQL from './GraphQL.svg';
+import EditPencil from './EditPencil.svg';
+import Environment from './Environment.svg';
 import FirewallOn from './Firewall-On.svg';
 import Gear from './Gear.svg';
+import GraphQL from './GraphQL.svg';
 import Group from './Group.svg';
+import HelpCenter from './HelpCenter.svg';
+import Home from './Home.svg';
 import Lock from './Lock.svg';
 import Mail from './Mail.svg';
 import MapPin from './MapPin.svg';
+import Planet from './Planet.svg';
 import Search from './Search.svg';
 import SpeedMeter from './Speed-Meter.svg';
 import Trashcan from './Trashcan.svg';
 import TreeView from './TreeView.svg';
-import EditPencil from './EditPencil.svg';
-import Environment from './Environment.svg';
-import Delete from './Delete.svg';
-import Planet from './Planet.svg';
 
 export {
   Alert,
   Check,
   ChevronDown,
   CloudDatabase,
+  Delete,
   Diagram,
   Dots,
-  GraphQL,
+  EditPencil,
+  Environment,
   FirewallOn,
   Gear,
+  GraphQL,
   Group,
+  HelpCenter,
+  Home,
   Lock,
   Mail,
   MapPin,
+  Planet,
   Search,
   SpeedMeter,
   Trashcan,
   TreeView,
-  EditPencil,
-  Environment,
-  Delete,
-  Planet,
 };
 

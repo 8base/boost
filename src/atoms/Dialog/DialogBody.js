28eb1730eb2daff1e1407a8d38471bd9cb6b460e
zouxuoz <zouxuoz@gmail.com>
2018-11-02 18:00:54 +0300

    fix(Dialog): limit max height for scrollable

--- src/atoms/Dialog/DialogBody.js
+++ src/atoms/Dialog/DialogBody.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
-import { createTheme } from '../../utils';
+import { createTheme, createStyledTag } from '../../utils';
 import { Card } from '../Card';
 import type { PropSizes } from '../../types';
 
 type DialogBodyProps = {|
   children?: React$Node,
   padding?: PropSizes,
+  scrollable?: boolean,
 |};
 
 const name = 'dialogBody';
 
 const theme = createTheme(name, {
   modifiers: {
+    scrollable: {
+      maxHeight: 'calc(60vh - 14rem)',
+    },
   },
   defaults: {
+    scrollable: false,
   },
 });
 
-function DialogBody({
-  children,
-  ...rest
-  }: DialogBodyProps) {
-  return <Card.Body { ...rest }>{ children }</Card.Body>;
-}
+const DialogBodyTag = createStyledTag(name, {});
+
+const DialogBody = ({ children, ...rest }: DialogBodyProps) => (
+  <DialogBodyTag tagName={ Card.Body } { ...rest }>
+    { children }
+  </DialogBodyTag>
+);
 
 export { DialogBody, theme };

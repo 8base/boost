630f6cc3b6a3fd309ae9f5cffc7f4210f699cc41
zouxuoz <zouxuoz@gmail.com>
2018-11-06 12:25:36 +0300

    fix(DateInput): static time for story

--- src/atoms/dataEntry/DateInput/DateInput.stories.js
+++ src/atoms/dataEntry/DateInput/DateInput.stories.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import moment from 'moment';
-
-let value = moment().format('YYYY-MM-DD');
 
 export default (asStory) => {
   asStory('ATOMS/DATA ENTRY/DateInput', module, (story, { DateInput }) => {
     story
       .add('default', () => (
-        <DateInput value={ value } onChange={ (val) => { value = val; } } />
+        <DateInput value="11/02/2018" onChange={ () => null } />
       ));
   });
 };

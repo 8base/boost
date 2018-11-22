efc8e22386c4a7c5615f1ca1c02921d356a5f313
zouxuoz <zouxuoz@gmail.com>
2018-11-22 18:23:45 +0300

    fix(Dialog): update stories

--- src/atoms/Dialog/Dialog.stories.js
+++ src/atoms/Dialog/Dialog.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default (asStory) => {
-  asStory('ATOMS/Dialog (skip shot)', module, (story, { ModalConsumer, Dialog, Paragraph, Button }) => {
+  asStory('ATOMS/Dialog (skip shot)', module, (story, { ModalContext, Dialog, Paragraph, Button }) => {
     story
-      .add('with default header, body, actions and footer (skip shot)', () => (
-        <Dialog.Plate id="ID" size="sm">
+      .add('with default header, body, actions and footer', () => (
+        <Dialog.Plate size="sm" isOpen>
           <Dialog.Header title="Mark Job as Completed" />
           <Dialog.Body>
             <Paragraph>
@@ -18,31 +18,33 @@ export default (asStory) => {
         </Dialog.Plate>
       ))
       .add('with state', () => (
-        <React.Fragment>
-          <Dialog.Plate id="ID" size="sm">
-            {
-              ({ args, onClose }) => (
-                <React.Fragment>
-                  <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
-                  <Dialog.Body>
-                    <Paragraph>
-                      Fagelia cancrivorous { args.foo } Curucaneca Echinocaris { args.bar } glassful agronomics
-                    </Paragraph>
-                  </Dialog.Body>
-                  <Dialog.Footer>
-                    <Button color="neutral" variant="outlined" text="Cancel" onClick={ onClose } />
-                    <Button type="submit" text="Apply" />
-                  </Dialog.Footer>
-                </React.Fragment>
-              )
-            }
-          </Dialog.Plate>
-          <ModalConsumer>
-            {
-              ({ openModal }) => <Button onClick={ () => openModal('ID', { foo: '00', bar: '00' }) }>Open</Button>
-            }
-          </ModalConsumer>
-        </React.Fragment>
+        <ModalContext.Consumer>
+          {
+            ({ openModal, closeModal }) => (
+              <React.Fragment>
+                <Dialog.Plate id="ID" size="sm" onClose={ () => closeModal('ID') }>
+                  {
+                    ({ args, onClose }) => (
+                      <React.Fragment>
+                        <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+                        <Dialog.Body>
+                          <Paragraph>
+                            Fagelia cancrivorous { args.foo } Curucaneca Echinocaris { args.bar } glassful agronomics
+                          </Paragraph>
+                        </Dialog.Body>
+                        <Dialog.Footer>
+                          <Button color="neutral" variant="outlined" text="Cancel" onClick={ onClose } />
+                          <Button type="submit" text="Apply" />
+                        </Dialog.Footer>
+                      </React.Fragment>
+                    )
+                  }
+                </Dialog.Plate>
+                <Button onClick={ () => openModal('ID', { foo: '00', bar: '00' }) }>Open</Button>
+              </React.Fragment>
+            )
+          }
+        </ModalContext.Consumer>
       ));
   });
 };

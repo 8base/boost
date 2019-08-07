7da0e23cfc086b1bd6379cdd83e3a928ab856135
Alexey Ryabov <lesha_12_01@mail.ru>
2019-08-07 17:06:38 +0300

    chore: add ability to change <Loader /> color

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import { LoaderTag, LoaderWrapperTag } from './Loader.theme';
+import { COLORS } from '../../theme';
 
 // $FlowIgnore
 import LoaderSvg from './Loader.svg';
@@ -9,29 +10,27 @@ import LoaderSvg from './Loader.svg';
 type LoaderProps = {
   size?: 'sm' | 'md' | 'lg',
   stretch?: boolean,
+  color?: $Keys<typeof COLORS>,
 };
 
 function Loader(props: LoaderProps) {
-  if (props.stretch) {
-    return (
-      <LoaderWrapperTag role="loader">
-        <LoaderTag { ...props } tagName="div">
-          <LoaderSvg width="100%" height="100%" />
-        </LoaderTag>
-      </LoaderWrapperTag>
-    );
-  }
-
-  return (
+  const uiLoader = (
     <LoaderTag { ...props } tagName="div" role="loader">
       <LoaderSvg width="100%" height="100%" />
     </LoaderTag>
   );
+
+  if (props.stretch) {
+    return <LoaderWrapperTag role="loader">{ uiLoader }</LoaderWrapperTag>;
+  }
+
+  return uiLoader;
 }
 
 Loader.defaultProps = {
   stretch: false,
   size: 'md',
+  color: 'RED',
 };
 
 export { Loader };

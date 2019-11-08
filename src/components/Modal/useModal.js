d1ffab3e6771fefdc0a77e818b4e8e424d395bfa
Osipov Vladimir <zouxuoz@gmail.com>
2019-11-08 11:30:46 +0300

    feat(Modal): pass dialog args

--- src/components/Modal/useModal.js
+++ src/components/Modal/useModal.js
@@ -1,13 +1,17 @@
 import { useContext } from 'react';
+import _ from 'lodash';
 
 import { ModalContext } from './ModalContext';
 
-function useModal() {
+function useModal(id) {
   const dialogContext = useContext(ModalContext);
+
+  const args = _.get(dialogContext, ['state', id, 'args'], {});
 
   return {
     openModal: dialogContext.openModal,
     closeModal: dialogContext.closeModal,
+    args,
   };
 }
 

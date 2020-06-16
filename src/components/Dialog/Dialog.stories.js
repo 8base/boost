e8852676d8470f222b1045713e9c6be624e1eb0a
Vyacheslav 'SLEL' Solomin <slel64222@gmail.com>
2020-06-16 23:21:52 +0300

    fix(docs): add contril button to dialog

--- src/components/Dialog/Dialog.stories.js
+++ src/components/Dialog/Dialog.stories.js
@@ -8,20 +8,36 @@ export default {
 };
 
 export const defaultStory = () => (
-  <Dialog size="sm" isOpen data-e2e-id="default-dialog">
-    <Dialog.Header title="Mark Job as Completed" />
-    <Dialog.Body>
-      <Paragraph>
-        Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
-      </Paragraph>
-    </Dialog.Body>
-    <Dialog.Footer>
-      <Button color="neutral" variant="outlined">
-        Cancel
-      </Button>
-      <Button type="submit">Apply</Button>
-    </Dialog.Footer>
-  </Dialog>
+  <ModalContext.Consumer>
+    { ({ openModal, closeModal }) => (
+      <React.Fragment>
+        <Dialog
+          size="sm"
+          onClose={ () => closeModal('default-dialog-id') }
+          id="default-dialog-id"
+          data-e2e-id="default-dialog"
+        >
+          { ({ onClose }) => (
+            <React.Fragment>
+              <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+              <Dialog.Body>
+                <Paragraph>
+                  Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
+                </Paragraph>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button color="neutral" variant="outlined" onClick={ onClose }>
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={ onClose }>Apply</Button>
+              </Dialog.Footer>
+            </React.Fragment>
+          ) }
+        </Dialog>
+        <Button onClick={ () => openModal('default-dialog-id') }>Open default dialog</Button>
+      </React.Fragment>
+    ) }
+  </ModalContext.Consumer>
 );
 
 defaultStory.story = {
@@ -29,20 +45,37 @@ defaultStory.story = {
 };
 
 export const withStretch = () => (
-  <Dialog stretch size="sm" isOpen data-e2e-id="default-dialog">
-    <Dialog.Header title="Mark Job as Completed" />
-    <Dialog.Body>
-      <Paragraph>
-        Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
-      </Paragraph>
-    </Dialog.Body>
-    <Dialog.Footer>
-      <Button color="neutral" variant="outlined">
-        Cancel
-      </Button>
-      <Button type="submit">Apply</Button>
-    </Dialog.Footer>
-  </Dialog>
+  <ModalContext.Consumer>
+    { ({ openModal, closeModal }) => (
+      <React.Fragment>
+        <Dialog
+          stretch
+          size="sm"
+          onClose={ () => closeModal('withStretch-dialog-id') }
+          id="withStretch-dialog-id"
+          data-e2e-id="default-dialog"
+        >
+          { ({ onClose }) => (
+            <React.Fragment>
+              <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+              <Dialog.Body>
+                <Paragraph>
+                  Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
+                </Paragraph>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button color="neutral" variant="outlined" onClick={ onClose }>
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={ onClose }>Apply</Button>
+              </Dialog.Footer>
+            </React.Fragment>
+          ) }
+        </Dialog>
+        <Button onClick={ () => openModal('withStretch-dialog-id') }>Open dialog with stretch</Button>
+      </React.Fragment>
+    ) }
+  </ModalContext.Consumer>
 );
 
 withStretch.story = {
@@ -50,20 +83,35 @@ withStretch.story = {
 };
 
 export const withLargeSize = () => (
-  <Dialog size="xl" isOpen data-e2e-id="default-dialog">
-    <Dialog.Header title="Mark Job as Completed" />
-    <Dialog.Body>
-      <Paragraph>
-        Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
-      </Paragraph>
-    </Dialog.Body>
-    <Dialog.Footer>
-      <Button color="neutral" variant="outlined">
-        Cancel
-      </Button>
-      <Button type="submit">Apply</Button>
-    </Dialog.Footer>
-  </Dialog>
+  <ModalContext.Consumer>
+    { ({ openModal, closeModal }) => (
+      <React.Fragment>
+        <Dialog size="xl"
+          onClose={ () => closeModal('withLargeSize-dialog-id') }
+          id="withLargeSize-dialog-id"
+          data-e2e-id="default-dialog"
+        >
+          { ({ onClose }) => (
+            <React.Fragment>
+              <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+              <Dialog.Body>
+                <Paragraph>
+                  Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
+                </Paragraph>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button color="neutral" variant="outlined" onClick={ onClose }>
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={ onClose }>Apply</Button>
+              </Dialog.Footer>
+            </React.Fragment>
+          ) }
+        </Dialog>
+        <Button onClick={ () => openModal('withLargeSize-dialog-id') }>Open dialog with large size</Button>
+      </React.Fragment>
+    ) }
+  </ModalContext.Consumer>
 );
 
 withLargeSize.story = {
@@ -71,24 +119,40 @@ withLargeSize.story = {
 };
 
 export const withScroll = () => (
-  <Dialog size="sm" isOpen data-e2e-id="default-dialog">
-    <Dialog.Header title="Mark Job as Completed" />
-    <Dialog.Body scrollable>
-      <Column>
-        { new Array(100).fill('').map(() => (
-          <Paragraph key="">
-            Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
-          </Paragraph>
-        )) }
-      </Column>
-    </Dialog.Body>
-    <Dialog.Footer>
-      <Button color="neutral" variant="outlined">
-        Cancel
-      </Button>
-      <Button type="submit">Apply</Button>
-    </Dialog.Footer>
-  </Dialog>
+  <ModalContext.Consumer>
+    { ({ openModal, closeModal }) => (
+      <React.Fragment>
+        <Dialog
+          size="sm"
+          onClose={ () => closeModal('withScroll-dialog-id') }
+          id="withScroll-dialog-id"
+          data-e2e-id="default-dialog"
+        >
+          { ({ onClose }) => (
+            <React.Fragment>
+              <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+              <Dialog.Body scrollable>
+                <Column>
+                  { new Array(100).fill('').map(() => (
+                    <Paragraph key="">
+                      Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
+                    </Paragraph>
+                  )) }
+                </Column>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button color="neutral" variant="outlined" onClick={ onClose }>
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={ onClose }>Apply</Button>
+              </Dialog.Footer>
+            </React.Fragment>
+          ) }
+        </Dialog>
+        <Button onClick={ () => openModal('withScroll-dialog-id') }>Open dialog with scroll</Button>
+      </React.Fragment>
+    ) }
+  </ModalContext.Consumer>
 );
 
 withScroll.story = {
@@ -96,27 +160,44 @@ withScroll.story = {
 };
 
 export const withStretchScroll = () => (
-  <Dialog size="sm" isOpen data-e2e-id="default-dialog" stretch>
-    <Dialog.Header title="Mark Job as Completed" />
-    <Dialog.Body scrollable>
-      <Column>
-        { new Array(100).fill('').map(() => (
-          <Paragraph key="">
-            Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
-          </Paragraph>
-        )) }
-      </Column>
-    </Dialog.Body>
-    <Dialog.Footer>
-      <Button color="neutral" variant="outlined">
-        Cancel
-      </Button>
-      <Button type="submit">Apply</Button>
-    </Dialog.Footer>
-  </Dialog>
+  <ModalContext.Consumer>
+    { ({ openModal, closeModal }) => (
+      <React.Fragment>
+        <Dialog
+          stretch
+          size="sm"
+          onClose={ () => closeModal('withStretchScroll-dialog-id') }
+          id="withStretchScroll-dialog-id"
+          data-e2e-id="default-dialog"
+        >
+          { ({ onClose }) => (
+            <React.Fragment>
+              <Dialog.Header title="Mark Job as Completed" onClose={ onClose } />
+              <Dialog.Body scrollable>
+                <Column>
+                  { new Array(100).fill('').map(() => (
+                    <Paragraph key="">
+                      Fagelia cancrivorous Nahor Curucaneca Echinocaris intrafissural glassful agronomics
+                    </Paragraph>
+                  )) }
+                </Column>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button color="neutral" variant="outlined" onClick={ onClose }>
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={ onClose }>Apply</Button>
+              </Dialog.Footer>
+            </React.Fragment>
+          ) }
+        </Dialog>
+        <Button onClick={ () => openModal('withStretchScroll-dialog-id') }>Open dialog with stretch scroll</Button>
+      </React.Fragment>
+    ) }
+  </ModalContext.Consumer>
 );
 
-withScroll.story = {
+withStretchScroll.story = {
   name: 'with stretch scroll',
 };
 
